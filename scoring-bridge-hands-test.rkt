7fbd77817f7b062132b31dfed5acf33b845042d7
lgwarda <kukikarma@gmail.com>
2020-03-24 17:07:18 +0100

    Add test for suit-dist-points

--- scoring-bridge-hands-test.rkt
+++ scoring-bridge-hands-test.rkt
@@ -46,7 +46,12 @@
      (test-equal? "returns '(5 3 2 3)" (suit-counts '(sa s10 s7 s6 s2 hq hj h9 ck c4 dk d9 d3))
                   '(5 3 2 3))
      (test-equal? "returns '(5 1 2 5)" (suit-counts '(h3 d7 sk s3 c10 dq d8 s9 s4 d10 c7 d4 s2))
-                  '(5 1 2 5))))
+                  '(5 1 2 5))
+     (test-equal? "returns 1" (suit-dist-points 2) 1)
+     (test-equal? "returns 2" (suit-dist-points 1) 2)
+     (test-equal? "returns 3" (suit-dist-points 0) 3)
+     (test-equal? "returns 0" (suit-dist-points 7) 0)
+     ))
 
   (run-tests suite))
 
