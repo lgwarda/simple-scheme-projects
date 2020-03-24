d748ac8e623cf367cfceb61b4f321f5397111522
lgwarda <kukikarma@gmail.com>
2020-03-24 16:01:24 +0100

    Add test for suit-counts function

--- scoring-bridge-hands-test.rkt
+++ scoring-bridge-hands-test.rkt
@@ -36,11 +36,17 @@
      
      (test-equal? "returns 9" (high-card-points '(sa s10 hq ck c4)) 9)
      (test-equal? "returns 13" (high-card-points '(sa s10 s7 s6 s2 hq hj h9 ck c4 dk d9 d3)) 13)
+     
 
      (test-equal? "returns 2" (count-suit 's '(sa s10 hq ck c4)) 2) 
      (test-equal? "returns 2" (count-suit 'c '(sa s10 s7 s6 s2 hq hj h9 ck c4 dk d9 d3)) 2)
-     (test-equal? "returns 5" (count-suit 'd '(h3 d7 sk s3 c10 dq d8 s9 s4 d10 c7 d4 s2)) 5) 
-     ))
+     (test-equal? "returns 5" (count-suit 'd '(h3 d7 sk s3 c10 dq d8 s9 s4 d10 c7 d4 s2)) 5)
+
+     (test-equal? "returns '(2 1 2 0)" (suit-counts '(sa s10 hq ck c4)) '(2 1 2 0))
+     (test-equal? "returns '(5 3 2 3)" (suit-counts '(sa s10 s7 s6 s2 hq hj h9 ck c4 dk d9 d3))
+                  '(5 3 2 3))
+     (test-equal? "returns '(5 1 2 5)" (suit-counts '(h3 d7 sk s3 c10 dq d8 s9 s4 d10 c7 d4 s2))
+                  '(5 1 2 5))))
 
   (run-tests suite))
 
