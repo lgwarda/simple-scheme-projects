79311e6c6f0972c491ba41922dd408321e273aa0
lgwarda <kukikarma@gmail.com>
2020-03-25 07:50:16 +0100

    Add test for hand-dist-points and bridge-val functions

--- scoring-bridge-hands-test.rkt
+++ scoring-bridge-hands-test.rkt
@@ -51,7 +51,12 @@
      (test-equal? "returns 2" (suit-dist-points 1) 2)
      (test-equal? "returns 3" (suit-dist-points 0) 3)
      (test-equal? "returns 0" (suit-dist-points 7) 0)
-     ))
+
+     (test-equal? "returns 1" (hand-dist-points '(sa s10 s7 s6 s2 hq hj h9 ck c4 dk d9 d3)) 1)
+     (test-equal? "returns 3" (hand-dist-points '(h3 d7 sk s3 c10 dq d8 s9 s4 d10 c7 d4 s2)) 3)
+
+     (test-equal? "returns 14" (bridge-val '(sa s10 s7 s6 s2 hq hj h9 ck c4 dk d9 d3)) 14)
+     (test-equal? "returns 8" (bridge-val '(h3 d7 sk s3 c10 dq d8 s9 s4 d10 c7 d4 s2)) 8)))
 
   (run-tests suite))
 
