cf8033b0d34bed251f62bbd4261365054389b28e
lgwarda <kukikarma@gmail.com>
2020-05-14 21:09:02 +0200

    Add hw04/ Add grader, templates, tests and homework files implement dupls-removed you can run the autograder on your own machine: racket -tm grader.rkt -- hw1-tests.rkt hw1.rkt dupls-removed

---
+++ cs61as/homeworks/hw04/hw04.rkt
@@ -0,0 +1,75 @@
+#lang racket
+
+(require berkeley)
+(provide (all-defined-out))
+
+; Exercise 1 - Define dupls-removed
+
+; Sentence -> Sentence 
+(define (dupls-removed sent)
+  (cond [(empty? sent) '()]
+        [else (if (member? (first sent) (bf sent))
+                  (dupls-removed (bf sent))
+                  (se (first sent )(dupls-removed (bf sent))))]))
+
+; Exercise 2 - Define count-word
+
+(define (count-word sent wd)
+  ; Your code here
+  (error "Not yet implemented")
+)
+
+; Exercise 3
+
+(define (pigl wd)
+  (if (pl-done? wd)
+      (word wd 'ay)
+      (pigl (word (bf wd) (first wd)))))
+
+(define (pl-done? wd)
+  (vowel? (first wd)))
+
+(define (vowel? letter)
+  (member? letter '(a e i o u)))
+
+; Explain what would happen if you used new-if instead of if below.
+#|
+Your explanation here
+
+|#
+
+; Exercise 4 - Define squares
+
+(define (squares sent)
+  ; Your code here
+  (error "Not yet implemented")
+)
+
+; Exercise 5 - Define switch
+
+(define (switch sent)
+  ; Your code here
+  (error "Not yet implemented")
+)
+
+; Exercise 6 - Define ordered?
+
+(define (ordered? sent)
+  ; Your code here
+  (error "Not yet implemented")
+)
+
+; Exercise 7 - Define ends-e
+
+(define (ends-e sent)
+  ; Your code here
+  (error "Not yet implemented")
+)
+
+; Exercise 8
+
+#|
+
+Your explanation here
+
+|#
