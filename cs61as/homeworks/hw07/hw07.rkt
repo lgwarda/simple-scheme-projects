24491cb200e925fe6b731a659dec953e83ee19f1
lgwarda <kukikarma@gmail.com>
2020-06-02 10:46:35 +0200

    Fix a bug - reverse operator <= in div-interval function

--- cs61as/homeworks/hw07/hw07.rkt
+++ cs61as/homeworks/hw07/hw07.rkt
@@ -36,7 +36,7 @@
 ; SICP 2.10 - Modify div-interval
 
 (define (div-interval x y) 
-   (if (<= 0 (* (lower-bound y) (upper-bound y))) 
+   (if (>= 0 (* (lower-bound y) (upper-bound y))) 
        (error "Division error (interval spans 0)" y) 
        (mul-interval x  
                      (make-interval (/ 1. (upper-bound y)) 
