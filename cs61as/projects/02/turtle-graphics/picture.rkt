3baaa96fe5accc6e8f2d4331d3bc1c3c0099057f
lgwarda <kukikarma@gmail.com>
2020-06-11 13:57:29 +0200

    Fix a bug Add an argument to ycor-vect and fix the parentheses in add-vect and sub-vect

--- cs61as/projects/02/turtle-graphics/picture.rkt
+++ cs61as/projects/02/turtle-graphics/picture.rkt
@@ -184,11 +184,11 @@
 
 (define (add-vect v1 v2)
   (make-vect (+ (xcor-vect v1)(xcor-vect v2))
-             (+ (ycor-vect v1)(ycor-vect) v2)))
+             (+ (ycor-vect v1)(ycor-vect v2))))
 
 (define (sub-vect v1 v2)
   (make-vect (- (xcor-vect v1)(xcor-vect v2))
-             (- (ycor-vect v1)(ycor-vect) v2)))
+             (- (ycor-vect v1)(ycor-vect v2))))
 
 (define (scale-vect s v)
   (make-vect (* (xcor-vect v) s)
