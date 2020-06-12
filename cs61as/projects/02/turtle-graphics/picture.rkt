bf4f6fa144c07b389b8da900ca6f0c48555daece
lgwarda <kukikarma@gmail.com>
2020-06-12 12:12:12 +0200

    Implement two version of below - first procedure is analogous to the beside procedure beside - second version is like beside and suitable rotation operations

--- cs61as/projects/02/turtle-graphics/picture.rkt
+++ cs61as/projects/02/turtle-graphics/picture.rkt
@@ -326,10 +326,22 @@
 ;; Exercise 8
 
 (define (below painter1 painter2)
-  (error "not yet implemented"))
+  (let ([split-point (make-vect 0.0 0.5)])
+    (let ([paint-bottom
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              (make-vect 1.0 0.0) 
+                              split-point)]
+          [paint-up
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.5)
+                              (make-vect 0.0 1.0))])
+      (lambda (frame)(paint-bottom frame) (paint-up frame)))))
 
 (define (below-2 painter1 painter2)
-  (error "not yet implemented"))
+  (rotate90 (beside (rotate270 painter1)
+                    (rotate270 painter2))))
 
 ;; Exercise 9
 
